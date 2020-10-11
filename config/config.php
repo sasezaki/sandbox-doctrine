505e04f7f13b85e0f139ec5a327d42d100461604
sasezaki <sasezaki@gmail.com>
2020-10-11 10:08:10 +0900

    entity annotaion mapping setup

--- config/config.php
+++ config/config.php
@@ -9,14 +9,29 @@ return [
         'connection' => [
             'orm_default' => [
                 'params' => [
-                    'url' => 'mysql://root:password@127.0.0.1:3306/doctrine-sandbox'
+                    'url' => 'mysql://root:password@127.0.0.1:3306/doctrine_sandbox'
                 ],
             ],
         ],
         'driver' => [
             'orm_default' => [
                 'class' => MappingDriverChain::class,
+                'drivers' => [
+                    'App\\Entity' => 'app_entity',
+                ]
+            ],
+            'app_entity' => [
+                'class' => \Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
+                'cache' => 'filesystem',
+                'paths' => __DIR__ . '/../src/App/Entity',
             ],
         ],
+        'cache' => [
+            'filesystem' => [
+                'class' => \Doctrine\Common\Cache\FilesystemCache::class,
+                'directory' => 'data/cache/DoctrineCache',
+                'namespace' => 'psr-container-doctrine',
+            ],
+        ]
     ],
 ];
---
+++ src/App/Entity/Product.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="products")
+ */
+class Product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    public function getId() : int
+    {
+        return $this->id;
+    }
+
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
