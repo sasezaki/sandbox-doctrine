e6b7dd98019fbd2929435e41df166bf3db04274b
sasezaki <sasezaki@gmail.com>
2020-10-11 10:42:34 +0900

    cleanup

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
+sandbox for debugging, around Doctrine & phpstan-doctrine
 
 
-```
+-----
+
+```sh
 docker run -it --name doctrine-sandbox -e MYSQL_ROOT_PASSWORD=password -v doctrine-sandbox:/var/lib/mysql:cached  -p 3306:3306 -d mysql:8.0
 ```
 
@@ -15,3 +18,4 @@ create database doctrine_sandbox;
 ```shell
 ./vendor/bin/doctrine orm:schema-tool:create
 ```
+
--- config/container.php
+++ config/container.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 use Laminas\ServiceManager\ServiceManager;
 use Roave\PsrContainerDoctrine\EntityManagerFactory;
 
-// Standard config keys
-$container = new ServiceManager([
+return new ServiceManager([
     'factories' => [
         'doctrine.entity_manager.orm_default' => EntityManagerFactory::class,
     ],
@@ -13,6 +12,3 @@ $container = new ServiceManager([
         'config' => require __DIR__ . '/config.php'
     ]
 ]);
-
-
-return $container;
