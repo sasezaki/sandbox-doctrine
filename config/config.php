7c12fa3258de84624afc5fc079bc08139526ee98
sasezaki <sasezaki@gmail.com>
2024-06-23 10:39:01 +0900

    update to orm:^3

--- config/config.php
+++ config/config.php
@@ -2,36 +2,41 @@
 
 declare(strict_types=1);
 
-use Doctrine\Common\Persistence\Mapping\Driver\MappingDriverChain;
+// see https://github.com/Roave/psr-container-doctrine/blob/5.2.x/example/full-config.php
 
 return [
     'doctrine' => [
         'connection' => [
             'orm_default' => [
+                'driver_class' => \Doctrine\DBAL\Driver\PDO\MySQL\Driver::class,
                 'params' => [
-                    'url' => 'mysql://root:password@127.0.0.1:3306/doctrine_sandbox'
-                ],
+                    'driver'   => 'pdo_mysql',
+                    'user'     => 'root',
+                    'password' => 'password',
+                    'dbname'   => 'doctrine_sandbox',
+                    'host' => '127.0.0.1'
+                ]
             ],
         ],
         'driver' => [
             'orm_default' => [
-                'class' => MappingDriverChain::class,
+                'class' => \Doctrine\ORM\Mapping\Driver\AttributeDriver::class,
                 'drivers' => [
                     'App\\Entity' => 'app_entity',
                 ]
             ],
             'app_entity' => [
-                'class' => \Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
-                'cache' => 'filesystem',
+                'class' => \Doctrine\ORM\Mapping\Driver\AttributeDriver::class,
+                // 'cache' => 'filesystem',
                 'paths' => __DIR__ . '/../src/App/Entity',
             ],
         ],
-        'cache' => [
-            'filesystem' => [
-                'class' => \Doctrine\Common\Cache\FilesystemCache::class,
-                'directory' => 'data/cache/DoctrineCache',
-                'namespace' => 'psr-container-doctrine',
-            ],
-        ]
+//         'cache' => [
+//             'filesystem' => [
+//                 'class' => \Symfony\Component\Cache\Adapter\FilesystemAdapter::class,
+//                 'directory' => 'data/cache/DoctrineCache',
+//                 'namespace' => 'psr-container-doctrine',
+//             ],
+//         ]
     ],
 ];
